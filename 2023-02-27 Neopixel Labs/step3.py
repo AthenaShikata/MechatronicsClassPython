1e2241a0316ed771cf0e1596e78e7bf0c122d9a7
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-02-28 08:41:50 -0800

    2/28/23

--- 2023-02-27 Neopixel Labs/step3.py
+++ 2023-02-27 Neopixel Labs/step3.py
@@ -2,18 +2,6 @@
 #
 # SPDX-License-Identifier: MIT
 
-
-"""
-Blink example for boards with ONLY a NeoPixel LED (e.g. without a built-in red LED).
-Includes QT Py and various Trinkeys.
-Requires two libraries from the Adafruit CircuitPython Library Bundle.
-Download the bundle from circuitpython.org/libraries and copy the
-following files to your CIRCUITPY/lib folder:
-* neopixel.mpy
-* adafruit_pixelbuf.mpy
-Once the libraries are copied, save this file as code.py to your CIRCUITPY
-drive to run it.
-"""
 import time
 import board
 import neopixel
@@ -26,11 +14,11 @@ pixels = neopixel.NeoPixel(board.NEOPIXEL, 3)
 
 
 while True:
-    # indexes the first element of the 'pixels' list and points to the
-    # first 3 Neopixels in the 'pixels' list
-    pixels[0]=(255, 0, 0)
-    pixels[1]=(0, 255, 0)
-    pixels[2]=(0, 0, 255)
-    time.sleep(0.5) #delay equivalent
-    pixels.fill((0, 0, 0)) #writes the Neopixels to off
-    time.sleep(0.5)
+    # indexes the first element of the 'pixels' list and points to the
+    # first 3 Neopixels in the 'pixels' list
+    pixels[0]=(255, 0, 0)
+    pixels[1]=(0, 255, 0)
+    pixels[2]=(0, 0, 255)
+    time.sleep(0.5) #delay equivalent
+    pixels.fill((0, 0, 0)) #writes the Neopixels to off
+    time.sleep(0.5)
--- 2023-02-27 Neopixel Labs/step4.py
+++ 2023-02-27 Neopixel Labs/step4.py
@@ -1,48 +1,23 @@
-# SPDX-FileCopyrightText: 2021 Kattni Rembor for Adafruit Industries
-#
-# SPDX-License-Identifier: MIT
-
-
-"""
-Blink example for boards with ONLY a NeoPixel LED (e.g. without a built-in red LED).
-Includes QT Py and various Trinkeys.
-Requires two libraries from the Adafruit CircuitPython Library Bundle.
-Download the bundle from circuitpython.org/libraries and copy the
-following files to your CIRCUITPY/lib folder:
-* neopixel.mpy
-* adafruit_pixelbuf.mpy
-Once the libraries are copied, save this file as code.py to your CIRCUITPY
-drive to run it.
-"""
 import time
 import board
 import neopixel
-# Calls the circuit Python neopixel library, specifies the default board
-# pins for the Neopixels, and the number of neopixels to access.  Returns a
-# list 'pixels' of indexable neopixles
+# Imports required libraries needed to function
+
+# Specifies the board, NeoPixel pins, and number of NeoPixels to access.
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
 
-while True:
-    # indexes the first element of the 'pixels' list and points to the
-    # # first 10 Neopixels in the 'pixels' list
-    pixels[1] #black
-    pixels[2] #grey
-    pixels[3] #white
-    pixels[4] #light purple
-    pixels[5] #purple
-
-    pixels[6] #black
-    pixels[7] #grey
-    pixels[8] #white
-    pixels[9] #light green
-    pixels[10] #green
-
-
-    """
-    pixels[0]=(255, 0, 0)
-    pixels[1]=(0, 255, 0)
-    pixels[2]=(0, 0, 255)
-    time.sleep(0.5) #delay equivalent
-    pixels.fill((0, 0, 0)) #writes the Neopixels to off
-    time.sleep(0.5)
-    """
+while True: #loop
+    pixels.fill((0,0,0)) #sets all the NeoPixels blank
+    pixels[0]=(255,0,255) #sets all selected NeoPixels purple
+    pixels[2]=(255,0,255)
+    pixels[4]=(255,0,255)
+    pixels[6]=(255,0,255)
+    pixels[8]=(255,0,255)
+    time.sleep(1) #wait 1 second
+    pixels.fill((0,0,0)) #sets all the NeoPixels blank
+    pixels[1]=(0,200,0) #sets all selected NeoPixels green
+    pixels[3]=(0,200,0)
+    pixels[5]=(0,200,0)
+    pixels[7]=(0,200,0)
+    pixels[9]=(0,200,0)
+    time.sleep(1) #wait 1 second
