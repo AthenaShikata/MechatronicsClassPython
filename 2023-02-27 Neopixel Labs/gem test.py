55b3340701bed6ba59b05500eb841fec5ea71721
Athena Shikata <99115384+AthenaShikata@users.noreply.github.com>
2023-03-10 11:54:15 -0800

    Update gem test.py

--- 2023-02-27 Neopixel Labs/gem test.py
+++ 2023-02-27 Neopixel Labs/gem test.py
@@ -3,10 +3,15 @@ import board
 import neopixel
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10)
 
+pixels.fill((0,0,0))
 while True: 
-    pixels.fill((0,0,0))
-    pixels[0]=(128,246,253) #sets light blue (diamond)
-    pixels[1]=(128,246,253)
-    pixels[2]=(128,246,253)
-    pixels[3]=(128,246,253)
-    pixels[4]=(128,246,253)
+    pixels[0]=(80,246,253) #sets light blue (diamond)
+    pixels[1]=(80,246,253)
+    pixels[2]=(80,246,253)
+    pixels[3]=(80,246,253)
+    pixels[4]=(80,246,253)
+    pixels[5]=(80,246,253)
+    pixels[6]=(80,246,253)
+    pixels[7]=(80,246,253)
+    pixels[8]=(80,246,253)
+    pixels[9]=(80,246,253)
